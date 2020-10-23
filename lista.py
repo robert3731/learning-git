90a2a821b935e6c83924d3c40986263ff27b31ad
robert3731 <73348260+robert3731@users.noreply.github.com>
2020-10-23 16:25:17 +0200

    Change to greetins

--- lista.py
+++ lista.py
@@ -1,8 +1,1 @@
-lista_zakupow = {
-    'piekarnia': ['chleb', 'pączek', 'bułki'],
-    'warzywniak': ['marchew', 'seler', 'rukola']
-}
-for key in lista_zakupow:
-    lst = [x.capitalize() for x in lista_zakupow[key]]
-    print('Idę do {} kupię tam {}'.format(key.capitalize(), lst))
-print('Kupuję {} prodkuktów'.format(sum(len(lista_zakupow[key]) for key in lista_zakupow)))
+print('Greetings :D')
