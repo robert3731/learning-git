5d41244f57d1990e7777d0083a7c9f7a48cae94d
Robert Chaim <robak3731@gmail.com>
2020-10-23 15:18:34 +0200

    Add shopping list iteration

--- lista.py
+++ lista.py
@@ -2,3 +2,6 @@ lista_zakupow = {
     'piekarnia': ['chleb', 'pączek', 'bułki'],
     'warzywniak': ['marchew', 'seler', 'rukola']
 }
+for key in lista_zakupow:
+    lst = [x for x in lista_zakupow[key]]
+    print('Idę do {} kupię tam {}'.format(key, lst))
