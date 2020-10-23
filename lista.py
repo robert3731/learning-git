68c5ad29504649b2ec3c27b116fc90232709ff24
Robert Chaim <robak3731@gmail.com>
2020-10-23 15:23:13 +0200

    Change word capitalisation

--- lista.py
+++ lista.py
@@ -3,5 +3,5 @@ lista_zakupow = {
     'warzywniak': ['marchew', 'seler', 'rukola']
 }
 for key in lista_zakupow:
-    lst = [x for x in lista_zakupow[key]]
-    print('Idę do {} kupię tam {}'.format(key, lst))
+    lst = [x.capitalize() for x in lista_zakupow[key]]
+    print('Idę do {} kupię tam {}'.format(key.capitalize(), lst))
